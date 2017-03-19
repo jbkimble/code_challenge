d3638d7235fcf6b79a98c5ae8338a62d1d3370f1
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 16:43:55 -0600

    Unit test for unique_postcodes

--- spec/models/street_cafe_spec.rb
+++ spec/models/street_cafe_spec.rb
@@ -43,4 +43,21 @@ describe StreetCafe do
       end
     end
   end
+
+  describe ".unique_postcodes" do
+    it "returns only unique postcodes" do
+      StreetCafe.create(name: "Lennys Cafe", address:"123 Castle Drive", postcode: "LS1 FBN", chairs_num: 20)
+      StreetCafe.create(name: "Margeries Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 30)
+      StreetCafe.create(name: "Tammy's Egg Nest", address:"123 LeMond Drive", postcode: "LS2 TRH", chairs_num: 30)
+
+      cafes = StreetCafe.unique_postcodes
+      expect(cafes.length).to eq(2)
+    end
+  end
+
+  describe ".total_places" do
+    it "returns total cafes for a postcode" do
+
+    end
+  end
 end
