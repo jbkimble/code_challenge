b8c8bb99365b5a3aee64e9498bc83dfb2e911933
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 10:02:57 -0600

    Add method for percentage of all chairs at postcode

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -5,16 +5,25 @@ class StreetCafe < ApplicationRecord
   validates :chairs_num, presence: true
 
   def self.unique_postcodes
-    find_by_sql("SELECT DISTINCT postcode FROM street_cafes")
+    find_by_sql("SELECT DISTINCT postcode FROM street_cafes;")
   end
 
   def total_places
     StreetCafe.count_by_sql("SELECT COUNT(id) FROM street_cafes
-                             WHERE postcode = '#{self.postcode}'")
+                             WHERE postcode = '#{self.postcode}';")
   end
 
   def total_chairs
     StreetCafe.count_by_sql("SELECT SUM(chairs_num) FROM street_cafes
-                             WHERE postcode = '#{self.postcode}'")
+                             WHERE postcode = '#{self.postcode}';")
+  end
+
+  def percentage_of_all_chairs_at_postcode
+    # Beloq code returns '0' something to do with both individual queries returning integers instead of floats
+    percentage = StreetCafe.count_by_sql("SELECT SUM(chairs_num) FROM street_cafes
+                                        WHERE postcode = '#{self.postcode}';").to_f /
+                 StreetCafe.count_by_sql("SELECT SUM(chairs_num) FROM street_cafes;").to_f
+
+    (percentage * 100.0).round(2)
   end
 end
