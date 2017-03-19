889eafb3983b3ccc789e78a8fb76b2c142f93a95
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 09:33:05 -0600

    Add feature test

---
+++ spec/features/street_cafe_table_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "As a visitor to '/cafes'" do
+
+  scenario "It shows me a table with all the correct headers" do
+    visit '/cafes'
+
+    expect(page).to have_content("Street Cafes By Post Code")
+    expect(page).to have_content("post_code")
+    expect(page).to have_content("total_places")
+    expect(page).to have_content("total_chairs")
+    expect(page).to have_content("chairs_pct")
+    expect(page).to have_content("place_with_max_chairs")
+    expect(page).to have_content("max_chairs")
+  end
+
+end
