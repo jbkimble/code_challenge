a760c8c30eed57e62664e0a0600c580dcdb308ac
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 09:19:25 -0600

    Add category column to street_cafes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319150217) do
+ActiveRecord::Schema.define(version: 20170319151428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170319150217) do
     t.string  "address"
     t.string  "postcode"
     t.integer "chairs_num"
+    t.string  "category"
   end
 
 end
