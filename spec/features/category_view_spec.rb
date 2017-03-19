a9a5ddb1215446f7c6c94e4a914809f714f38174
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 12:00:55 -0600

    Finish testing category view

--- spec/features/category_view_spec.rb
+++ spec/features/category_view_spec.rb
@@ -27,14 +27,27 @@ describe "When I visit '/category'" do
 
   scenario "I see the number of cafes in each category" do
     StreetCafe.create(name: "Margeries Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 30, category: "ls1 large")
-    StreetCafe.create(name: "Mags Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 15, category: "ls1 large")
-    StreetCafe.create(name: "Mags Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 20, category: "ls1 large")
-    StreetCafe.create(name: "Mags Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 15, category: "other")
-    StreetCafe.create(name: "Mags Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 20, category: "other")
+    StreetCafe.create(name: "Mags Cafe", address:"1 LeMond Drive", postcode: "LS1 FBN", chairs_num: 15, category: "ls1 large")
+    StreetCafe.create(name: "Toms Cafe", address:"123 LeM Drive", postcode: "LS1 FBN", chairs_num: 20, category: "ls1 large")
+    StreetCafe.create(name: "Larry Cafe", address:"12 LeMond Drive", postcode: "LS1 FBN", chairs_num: 15, category: "other")
+    StreetCafe.create(name: "Meg Cafe", address:"123 LeMond Dr", postcode: "LS1 FBN", chairs_num: 20, category: "other")
 
     visit '/category'
 
     expect(page).to have_content("3")
     expect(page).to have_content("2")
+  end
+
+  scenario "I see the total chairs for each category" do
+    StreetCafe.create(name: "Margeries Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 30, category: "ls1 large")
+    StreetCafe.create(name: "Mags Cafe", address:"123 Le Drive", postcode: "LS1 FBN", chairs_num: 15, category: "ls1 large")
+    StreetCafe.create(name: "Tom Cafe", address:"123 LeMo Drive", postcode: "LS1 FBN", chairs_num: 20, category: "ls1 large")
+    StreetCafe.create(name: "Frank Cafe", address:"12 LeMond Drive", postcode: "LS1 FBN", chairs_num: 11, category: "other")
+    StreetCafe.create(name: "Lindas Cafe", address:"13 LeMond Drive", postcode: "LS1 FBN", chairs_num: 10, category: "other")
+
+    visit '/category'
+
+    expect(page).to have_content("65")
+    expect(page).to have_content("21")
   end
 end
