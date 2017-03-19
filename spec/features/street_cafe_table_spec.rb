8930ef9769b7e21be90e2e5a90c0bb774b4e5dce
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 09:40:26 -0600

    Add test for unique postcodes

--- spec/features/street_cafe_table_spec.rb
+++ spec/features/street_cafe_table_spec.rb
@@ -14,4 +14,14 @@ describe "As a visitor to '/cafes'" do
     expect(page).to have_content("max_chairs")
   end
 
+  scenario "It displays unique post codes" do
+    StreetCafe.create(name: "Lennys Cafe", address:"123 Castle Drive", postcode: "LS1 FBN", chairs_num: 20)
+    StreetCafe.create(name: "Margeries Cafe", address:"123 LeMond Drive", postcode: "LS1 FBN", chairs_num: 30)
+    StreetCafe.create(name: "Tammy's Egg Nest", address:"123 LeMond Drive", postcode: "LS2 TRH", chairs_num: 30)
+
+    visit '/cafes'
+
+    expect(page).to have_content("LS1 FBN")
+    expect(page).to have_content("LS2 TRH")
+  end
 end
