68e2784a5ccf440b78d0b8c1e9c0e6991dad4127
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-19 09:17:26 -0600

    Add validations

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -1,4 +1,7 @@
 class StreetCafe < ApplicationRecord
-  
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :postcode, presence: true
+  validates :chairs_num, presence: true
 
 end
